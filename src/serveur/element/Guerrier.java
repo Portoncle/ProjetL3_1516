70dedb20d58dbd0e0414e27acf8b04b60aceec5b
PALACIOS Nicolas <nicolas.palacios@hotmail.fr>
2016-01-04 18:13:37 +0100

    Premières définition des personnages

---
+++ src/serveur/element/Guerrier.java
@@ -0,0 +1,31 @@
+package serveur.element;
+
+/*
+	Guerrier
+    Santé 100 
+    Force 60-80
+    Init 20
+    Facultés
+        lent (se déplace d'une case les autres 3)
+    Comportement
+        Fonce sur l'ennmi capté, stuff après
+        Consomme direct une potion
+*/
+
+/*
+
+import java.util.Random;
+
+public class Guerrier extends Personnage{
+public Guerrier(){
+	this.setNom("Guerrier");
+	this.setVie(100);
+	
+	// Tirage d'un nombre aléatoire entre 60 et 80
+	Random rand = new Random();
+	int force = rand.nextInt(80 - 60 + 1) + 60;
+	this.setForce(force);
+	
+	this.setInitiative(20);
+}
+}*/
